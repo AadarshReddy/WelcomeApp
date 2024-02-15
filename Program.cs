cf257d76503a26eb5fe49921f5f367ff02c57ee5
Aadarsh Reddy <areddy5757@gmail.com>
2024-02-15 12:01:53 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomeApp;
+
+Console.WriteLine("Welcome  to App");
+signIn obj = new signIn();
+Console.WriteLine(obj.Authenticate("admin", "admin@123"));
